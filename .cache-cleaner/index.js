6af9e1f5ad8b7f00d523727ca0f538e613a20f8b
João Paulo Marquesini <jao@markezine.me>
2021-07-18 18:42:22 -0300

    adding log to cache cleaner

--- .cache-cleaner/index.js
+++ .cache-cleaner/index.js
@@ -1,7 +1,12 @@
 const { default: axios } = require("axios");
 
 async function run() {
-    await axios.get("https://purge.jsdelivr.net/gh/LittleLightForDestiny/littleLightData/cache_test.json");
+    await clean("https://purge.jsdelivr.net/gh/LittleLightForDestiny/littleLightData/cache_test.json");
+}
+
+async function clean(url) {
+    const result = await axios.get(url);
+    console.log(result.data);
 }
 
 run();
