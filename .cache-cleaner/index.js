8ab96ac1cb894e8520d15a2b962a574b0ab3cdc9
João Paulo Marquesini <jao@markezine.me>
2021-07-18 18:50:11 -0300

    updating cache cleaner script to clean actual files

--- .cache-cleaner/index.js
+++ .cache-cleaner/index.js
@@ -1,7 +1,9 @@
 const { default: axios } = require("axios");
 
 async function run() {
-    await clean("https://purge.jsdelivr.net/gh/LittleLightForDestiny/littleLightData/cache_test.json");
+    await clean("https://purge.jsdelivr.net/gh/LittleLightForDestiny/littleLightData/collaborators.json");
+    await clean("https://purge.jsdelivr.net/gh/LittleLightForDestiny/littleLightData/game_data.json");
+    await clean("https://purge.jsdelivr.net/gh/LittleLightForDestiny/littleLightData/popular_wishlists.json");
 }
 
 async function clean(url) {
